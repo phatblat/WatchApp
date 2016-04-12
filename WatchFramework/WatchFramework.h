7c01323458f6495345e0f23c393c3f03b3731543
Ben Chatelain <ben@octop.ad>
2016-04-11 20:23:10 -0600

    Add WatchFramework and tests targets

---
+++ WatchFramework/WatchFramework.h
@@ -0,0 +1,19 @@
+//
+//  WatchFramework.h
+//  WatchFramework
+//
+//  Created by Ben Chatelain on 4/11/16.
+//  Copyright © 2016 Ben Chatelain. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for WatchFramework.
+FOUNDATION_EXPORT double WatchFrameworkVersionNumber;
+
+//! Project version string for WatchFramework.
+FOUNDATION_EXPORT const unsigned char WatchFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WatchFramework/PublicHeader.h>
+
+
